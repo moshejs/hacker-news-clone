780c0700d4cbace1b5a8fbdde5e4117f4f266331
Moshe Malka <hello@moshemalka.com>
2024-05-02 14:02:45 -0400

    cleanup

--- src/components/StoryItem.tsx
+++ src/components/StoryItem.tsx
@@ -1,5 +1,4 @@
 'use client';
-import { useState } from 'react';
 import { formatDistanceToNow } from 'date-fns';
 import { useSavedStories } from '../contexts/SavedStoriesContext';
 
--- src/components/StoryList.tsx
+++ src/components/StoryList.tsx
@@ -57,6 +57,5 @@ export default function StoryList({ stories }: StoryListProps) {
         ))}
       </ul>
     </>
-
   );
 }
